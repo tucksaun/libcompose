986c7bc791ffc4af27618f29ca2f1255039213c3
louhde <juliehhourcade@gmail.com>
2018-06-27 11:52:26 +0200

    Debug

--- docker/container/container.go
+++ docker/container/container.go
@@ -16,6 +16,7 @@ import (
 	"github.com/docker/docker/api/types/network"
 	"github.com/docker/docker/client"
 	"github.com/docker/docker/pkg/stdcopy"
+	"github.com/docker/docker/pkg/term"
 	"github.com/docker/go-connections/nat"
 	"github.com/docker/libcompose/config"
 	"github.com/docker/libcompose/labels"
@@ -178,7 +179,7 @@ func (c *Container) Run(ctx context.Context, configOverride *config.ServiceConfi
 		errCh       chan error
 		out, stderr io.Writer
 		in          io.ReadCloser
-		//inFd        uintptr
+		inFd        uintptr
 	)
 
 	if configOverride.StdinOpen {
@@ -201,8 +202,7 @@ func (c *Container) Run(ctx context.Context, configOverride *config.ServiceConfi
 		return -1, err
 	}
 
-	logrus.Println("Don't open STDIN when TTY is true")
-	/*if configOverride.StdinOpen {
+	if configOverride.StdinOpen {
 		// set raw terminal
 		inFd, _ = term.GetFdInfo(in)
 		state, err := term.SetRawTerminal(inFd)
@@ -211,7 +211,7 @@ func (c *Container) Run(ctx context.Context, configOverride *config.ServiceConfi
 		}
 		// restore raw terminal
 		defer term.RestoreTerminal(inFd, state)
-	}*/
+	}
 
 	// holdHijackedConnection (in goroutine)
 	errCh = make(chan error, 1)
